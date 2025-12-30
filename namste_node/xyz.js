e93dd4a6d6149d2a8db05e4e58d1975019a30e19
sambitkumarjethy <sambitkumarjethy@gmail.com>
2025-12-30 20:04:48 +0530

    IIFE in node js

--- namste_node/xyz.js
+++ namste_node/xyz.js
@@ -1,1 +1,16 @@
 console.log("Console from xyz.js");
+// All the code of module is wraped inside a function(IIFE)
+//IIFE - Imediately invoked function Expression
+(function (module, require) {
+  // require(path);
+  // All code of module runs here
+})(); // Imedialty invoked function
+
+// #IIFE
+// -> Immediately invoke the code
+// -> It keeps variable and function prrivate/safe
+
+// Q - HOW ARE VARABLES AND FUNCTION PRIVATE IN DIFFERENT MODULE ?
+// ANS = IIFE AND require statement
+// Q - how do you get the access to module.export
+// ANS - nodejs passes module as parameter to IIFE
