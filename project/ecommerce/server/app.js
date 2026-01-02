25d194372ea9b447760454778141bcdecd578658
sambitkumarjethy <sambitkumarjethy@gmail.com>
2026-01-02 22:48:39 +0530

    database setup and create table using models

--- project/ecommerce/server/app.js
+++ project/ecommerce/server/app.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { config } from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import { createTables } from "./utils/createTables.js";
 
 const app = express();
-config({ path: "./config/config.env" });
+
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
@@ -17,11 +17,14 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   fileUpload({
     tempFileDir: "./uploads",
     useTempFiles: true,
   })
 );
+
+createTables();
 
 export default app;
--- project/ecommerce/server/server.js
+++ project/ecommerce/server/server.js
@@ -1,4 +1,23 @@
+import "./config/env.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config/config.env" });
+
 import app from "./app.js";
+import { v2 as cloudinary } from "cloudinary";
+import "./database/db.js";
+
+console.log("ENV DEBUG:", {
+  DB_USER: process.env.DB_USER,
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
 app.listen(process.env.PORT, () => {
-  console.log(`sever is running on port ${process.env.PORT}`);
+  console.log(`server is running on port ${process.env.PORT}`);
 });
