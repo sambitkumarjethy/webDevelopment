d8d3a0d1a1acf73e7abda848508d6afea79a382f
sambitkumarjethy <sambitkumarjethy@gmail.com>
2026-01-05 23:30:35 +0530

    login API call

--- project/ecommerce/server/controllers/authController.js
+++ project/ecommerce/server/controllers/authController.js
@@ -27,6 +27,25 @@ export const register = catchAsyncErrors(async (req, res, next) => {
   );
   sendToken(user.rows[0], 201, "User Registered successfuly", res);
 });
-export const login = catchAsyncErrors(async (req, res, next) => {});
+export const login = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Please provide email and password.", 400));
+  }
+
+  const user = await database.query(`SELECT * FROM users where email = $1`, [
+    email,
+  ]);
+
+  if (user.rowCount.length === 0) {
+    return next(new ErrorHandler("Invalid email or password.", 400));
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.rows[0].password);
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid email or password.", 400));
+  }
+  sendToken(user.rows[0], 201, "User logged In successfuly", res);
+});
 export const getUser = catchAsyncErrors(async (req, res, next) => {});
 export const logout = catchAsyncErrors(async (req, res, next) => {});
