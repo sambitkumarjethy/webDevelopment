27d1a330d9499a7c4f01811cd301f64e052b24c7
sambitkumarjethy <sambitkumarjethy@gmail.com>
2026-01-08 23:21:31 +0530

    executing a sync code file using node

---
+++ namste_node/async.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const https = require("https");
+
+console.log("Hello World");
+
+var a = 1073145;
+var b= 209896;
---
+++ namste_node/sync.js
@@ -0,0 +1,12 @@
+console.log("Hello Sambit");
+
+var a = 123456;
+var b = 9876543;
+
+function multiply(x, y) {
+  const result = a * b;
+  return result;
+}
+
+var c = multiply(a, b);
+console.log({ c });
