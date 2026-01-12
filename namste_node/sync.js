d2dee30a813ec17339cd169e816b12d1ab3d2ab8
sambitkumarjethy <sambitkumarjethy@gmail.com>
2026-01-12 08:23:12 +0530

    namstenode EP7-sync,async,setTimeoutzero

--- namste_node/sync.js
+++ namste_node/sync.js
@@ -1,7 +1,48 @@
+const fs = require("fs");
+const https = require("https");
 console.log("Hello Sambit");
 
 var a = 123456;
 var b = 9876543;
+
+fs.readFileSync("./file.txt", "utf-8", (err, data) => {
+  // blocks the main thread
+  console.log(`File data : ${data}`);
+});
+
+https.get("https://dummyjson.com/products/1", (res) => {
+  console.log("Fetched products in sync");
+});
+
+// https
+//   .get("https://dummyjson.com/products/1", (res) => {
+//     let data = "";
+
+//     // collect chunks
+//     res.on("data", (chunk) => {
+//       data += chunk;
+//     });
+
+//     // when response ends
+//     res.on("end", () => {
+//       const jsonData = JSON.parse(data);
+//       console.log("Fetched product:", jsonData);
+//     });
+//   })
+//   .on("error", (err) => {
+//     console.error("Error:", err.message);
+//   });
+
+// .then((res) => res.json())
+// .then(console.log);
+
+setTimeout(() => {
+  console.log("Settimeout 5 seconds.");
+}, 5000);
+
+fs.readFile("./file.txt", "utf-8", (err, data) => {
+  console.log(`File data : ${data}`);
+});
 
 function multiply(x, y) {
   const result = a * b;
