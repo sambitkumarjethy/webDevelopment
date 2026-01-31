d5af4dde4637739101dbcf3ad3ba59261a51d32a
sambitkumarjethy <sambitkumarjethy@gmail.com>
2026-01-31 23:04:00 +0530

    Ecomm -notes added at client

--- project/ecommerce/client/src/components/Layout/Navbar.jsx
+++ project/ecommerce/client/src/components/Layout/Navbar.jsx
@@ -2,8 +2,12 @@ import { Menu, User, ShoppingCart, Sun, Moon, Search } from "lucide-react";
 import { useTheme } from "../../contexts/ThemeContext";
 import { useDispatch, useSelector } from "react-redux";
 
+// const Navbar = () => {
+//   return <></>;
+// };
+
 const Navbar = () => {
-  return <></>;
+  const dispatch = useDispatch();
 };
 
 export default Navbar;
