6e5ddf5acc17b70850a980919c995c32e76c242b
sambitkumarjethy <sambitkumarjethy@gmail.com>
2026-07-31 20:44:00 +0530

    Tailwind css configured

--- project/fundamentals/src/App.jsx
+++ project/fundamentals/src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <>React fundamentals</>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <div className="bg-white p-8 rounded-xl shadow-lg">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Tailwind CSS is Working! 🎉
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
